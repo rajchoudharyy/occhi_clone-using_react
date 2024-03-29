0d6def0a51be9b57275560d0ebfb8d973568b282
rajchoudharyy <rajburdak3@gmail.com>
2024-03-29 20:21:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navabr from './Components/Navabr'
+import LandingPage from './Components/LandingPage'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-900 text-white'>
+      <Navabr/>
+      <LandingPage/>
+    </div>
+  )
+}
+
+export default App
